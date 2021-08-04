0a9ae1d7180db01bd91ac793f98105426249ff09
renugadevi-2613 <renugadevi@bluemeric.com>
2021-08-04 16:58:09 +0530

    README updates with logo and twitter handle

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
+<img src="https://gopaddle-marketing.s3.ap-southeast-2.amazonaws.com/Configurator-sync-service.png" width="50%">
+
+___
+
+
 [![StackShare](http://img.shields.io/badge/tech-stack-0690fa.svg?style=flat)](https://stackshare.io/gopaddleio/gopaddle)
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
 
 # Configurator
 
 Configurator is a sync service that keeps Kubernetes deployments in sync with the ConfigMap updates. 
-
-[<img src="https://gopaddle-marketing.s3-ap-southeast-2.amazonaws.com/addo-configurator.png" width="80%">](https://youtu.be/sTX8RASHMXQ?start=2:30:30)
 
 # Supported Versions
 
@@ -75,10 +79,4 @@ spec:
 ```
 Whenever the customConfigMap is updated, Configurator will create a configMap with a new postfix and will automatically sync up the related deployments with the newly created configMap.
 
-Same functionality applies for secrets as well.
-
-License
-----
-
-Apache License 2.0
-
+Same functionality applies for secrets as well 
--- watcher/watcher.go
+++ watcher/watcher.go
@@ -58,8 +58,68 @@ func StartWatcher(label WatcherLabel, prevConfig, newConfig string) {
 		listOpts.LabelSelector = secretLabel
 	}
 
+	//var watch watch.Interface
+	//var er error
+	// if label.ConfigMap != "" {
+	// 	for {
+	// 		watch, er = clientSet.CoreV1().ConfigMaps(label.NameSpace).Watch(context.TODO(), listOpts)
+	// 		if er != nil {
+	// 			log.Println("failed on watching configmap label '%s'", label.ConfigMap)
+	// 			log.Println("error in wtach --->", er.Error())
+	// 			continue
+	// 		} else {
+	// 			break
+	// 		}
+	// 	}
+	// } else if label.Secret != "" {
+	// 	for {
+	// 		watch, er = clientSet.CoreV1().Secrets(label.NameSpace).Watch(context.TODO(), listOpts)
+	// 		if er != nil {
+	// 			log.Println("failed on watching configmap label '%s'", label.Secret)
+	// 			log.Println("error in wtach --->", er.Error())
+	// 			continue
+	// 		} else {
+	// 			break
+	// 		}
+	// 	}
+	// }
+
 	previousName := prevConfig
 	name := newConfig
+	// previousCreatedTime := ""
+	// layout := "2006-01-02T15:04:05Z"
+	// for {
+	//result := <-watch.ResultChan()
+	// if result.Type == "ADDED" {
+	// 	data, errs := json.Marshal(result.Object)
+	// 	if errs != nil {
+	// 		log.Println("Failed to marshal", errs, time.Now().UTC())
+	// 	}
+	// 	var f interface{}
+	// 	if err := json.Unmarshal(data, &f); err != nil {
+	// 		log.Println("Failed to unmarshal", err, time.Now().UTC())
+	// 	}
+	// 	object := f.(map[string]interface{})
+	// 	//geting metadata
+	// 	metadata := object["metadata"].(map[string]interface{})
+	// 	name := metadata["name"].(string)
+
+	// 	if previousName == "" {
+	// 		previousName = name
+	// 		previousCreatedTime = metadata["creationTimestamp"].(string)
+	// 	}
+	// 	pretime, err := time.Parse(layout, previousCreatedTime)
+	// 	if err != nil {
+	// 		log.Println("failed on parse time", err, time.Now().UTC())
+	// 	}
+	// 	currenCreatedTime, er := time.Parse(layout, metadata["creationTimestamp"].(string))
+	// 	if er != nil {
+	// 		log.Println("failed on parse time", er, time.Now().UTC())
+	// 	}
+	// 	if pretime.Equal(currenCreatedTime) {
+	// 		continue
+	// 	} else if currenCreatedTime.After(pretime) {
+	// 		previousCreatedTime = metadata["creationTimestamp"].(string)
 	labelStr := SplitStr(previousName)
 	var opts metav1.ListOptions
 	opts.LabelSelector = labelStr
@@ -305,6 +365,15 @@ func StartWatcher(label WatcherLabel, prevConfig, newConfig string) {
 		}
 	}
 	previousName = name
+	//}
+	// } else if result.Type == "MODIFIED" || result.Type == "DELETED" {
+	// 	continue
+	// } else {
+	// 	watch.Stop()
+	// 	break
+	// }
+	// }
+	// watch.Stop()
 }
 
 // Start the watcher if any previous labels were present
